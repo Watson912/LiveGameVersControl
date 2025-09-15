d2d03e5ab6fbeda241ed7b9d94c367ec3bcd4301
Shiso <165101027+Watson912@users.noreply.github.com>
2025-09-15 01:03:44 -0700

    handle the wall collision using signal

--- Scripts/EnemyIdle.gd
+++ Scripts/EnemyIdle.gd
@@ -2,26 +2,26 @@ extends State
 class_name EnemyIdle
 
 @export var enemy : CharacterBody3D
-@export var moveSpeed : float = 10.0
-
-
-var move_direction : Vector3
+var EnemyScript
+var enemyScript
 var wander_time : float
 
 func randomize_wander():
-	#move_direction = Vector3(randf_range(-1,1), 0, randf_range(-1,1)).normalized()
+	var move_direction = enemyScript.get_move_direction()
+	move_direction = Vector3(randf_range(-1,1), 0, randf_range(-1,1)).normalized()
 	wander_time = randf_range(0, 1)
 	
 func Enter():
+	EnemyScript = load("res://Scripts/enemy.gd")
+	enemyScript = EnemyScript.new()
 	randomize_wander()
 	
+	
 func Update(delta: float):
-	print(enemy.velocity)
 	if wander_time > 0:
 		wander_time -= delta
 		
 	else:
-		pass
 		randomize_wander()
 		
 func Physics_Update(delta: float):
@@ -30,8 +30,6 @@ func Physics_Update(delta: float):
 		
 func move_around(time: float):
 	
-	move_direction = Vector3(0,0,-1)
-	enemy.velocity = move_direction * moveSpeed
-	
-
+	#move_direction = Vector3(0,0,-1)
+	enemy.velocity = enemyScript.get_move_direction() * enemyScript.get_move_speed()
 	
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -4,10 +4,14 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var player = null
 var turning = false
+var EnemyIdle
 var turn_target_angle = 0.0
+var move_direction : Vector3
 @onready var ray_cast_3d = $"../../RayCast3D"
 @onready var shape_cast_3d = $"../../ShapeCast3D"
 
+@export var enemy : CharacterBody3D
+@export var moveSpeed : float = 10.0
 
 @onready var head = $head
 @export var player_path : NodePath
@@ -16,7 +20,8 @@ var turn_target_angle = 0.0
 
 func _ready():
 	player = get_node(player_path)
-
+	EnemyIdle = load("res://Scripts/EnemyIdle.gd")
+	EnemyIdle = EnemyIdle.new()
 
 func _physics_process(delta: float) -> void:
 	# Using the navigationAgent3d
@@ -27,29 +32,34 @@ func _physics_process(delta: float) -> void:
 
 	#look_at(Vector3(global_position.x + velocity.x, global_position.y, global_position.z + velocity.z), Vector3.UP)
 	
-	var facing = -enemy.global_transform.basis.tdotz(move_direction)
-	var fov = cos(deg_to_rad(120))
-	if enemy.is_on_wall() and not turning:
-		if shape_cast_3d.is_colliding():
-			if facing > fov: 
-				turn_around(delta)
-				print(enemy.velocity)
-			enemy.velocity = moveSpeed * move_direction
+	#var facing = -global_transform.basis.tdotz(EnemyIdle.get_move_direction)
+	#var fov = cos(deg_to_rad(120))
+	#if is_on_wall() and not turning:
+		#if shape_cast_3d.is_colliding():
+			#if facing > fov: 
+				##turn_around(delta)
+				#print(velocity)
+			#velocity = EnemyIdle.get_move_direction() * moveSpeed
 			
 	move_and_slide()
 	
-func turn_around(delta: float):
-	
-	# When the enemy hits the wall, they should stop and turn around before moving again
-	turning = true 
-	var dir = move_direction
-	move_direction = Vector3.ZERO
-	
-	var tween = create_tween()
-	tween.tween_property(enemy, "rotation_degrees", Vector3(0, 90,0), 0.2).as_relative()
-	
-	await tween.finished
-	
-	move_direction * -1
-	turning = false
-	
+#func turn_around(delta: float):
+	#
+	## When the enemy hits the wall, they should stop and turn around before moving again
+	#turning = true 
+	#var dir = move_direction
+	#move_direction = Vector3.ZERO
+	#
+	#var tween = create_tween()
+	#tween.tween_property(enemy, "rotation_degrees", Vector3(0, 90,0), 0.2).as_relative()
+	#
+	#await tween.finished
+	#
+	#move_direction * -1
+	#turning = false
+
+func get_move_speed():
+	return moveSpeed
+
+func get_move_direction():
+	return move_direction
