d5a75df6e53663ae3c1aa3155d552d324b4c531b
Shiso <165101027+Watson912@users.noreply.github.com>
2025-09-17 00:18:08 -0700

    changed file'

--- Scripts/EnemyIdle.gd
+++ Scripts/EnemyIdle.gd
@@ -2,9 +2,12 @@ extends State
 class_name EnemyIdle
 
 @export var enemy : CharacterBody3D
+@export var move_speed := 10.0
 var move_direction : Vector3
-var move_speed := 10.0
 var wander_time : float
+@onready var ray_cast: RayCast3D = %rayCast
+
+signal turn_around
 
 func randomize_wander():
 	move_direction = Vector3(randf_range(-1,1), 0, randf_range(-1,1)).normalized()
@@ -14,22 +17,24 @@ func Enter():
 	
 	randomize_wander()
 	
-	
 func Update(delta: float):
-	if wander_time > 0:
-		wander_time -= delta
-		
-	else:
-		randomize_wander()
-		
-	enemy.look_at(Vector3(enemy.velocity.x, enemy.global_position.y, enemy.velocity.z), Vector3.UP)
 	
+		if wander_time > 0:
+			wander_time -= delta
+			
+		else:
+			randomize_wander()
+		
+		
+			
+		enemy.look_at(Vector3(enemy.velocity.x, enemy.global_position.y, enemy.velocity.z), Vector3.UP)
+		
 func Physics_Update(delta: float):
+
 	if enemy:
-		move_around(delta)
+		enemy.velocity = move_direction * move_speed
+	
+	if enemy.is_on_wall():
+		randomize_wander()
+	 
 		
-func move_around(time: float):
-	
-	#move_direction = Vector3(0,0,-1)
-	enemy.velocity = move_direction * move_speed
-	
